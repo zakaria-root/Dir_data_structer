2ecaffc8d79a7bffb22d02e842742196f5ef64e1
zakaria-root <elmourtazakezia@gmail.com>
2022-04-09 16:59:27 +0000

    fat : add the main file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "implementationContact.h"
+#include "implementationDir.h"
+#include "implementationList.h"
+#include "implementationNode.h"
+
+main()
+{
+    std::cout << "the main file" << std::endl;
+}
