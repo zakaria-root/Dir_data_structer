a6153104bf7f40651a8ab2363531f0f24c13f4bb
zakaria-root <elmourtazakezia@gmail.com>
2022-04-09 16:10:14 +0000

    feat: add the contatc implementaition

---
+++ implementationContact.h
@@ -0,0 +1,35 @@
+#include "dir.h"
+#include <string.h>
+
+Contact::Contact(string name, string email, string phone = 0)
+{
+    this->name = name;
+    this->email = email;
+    this->phone = phone;
+}
+
+Contact::Contact(const Contact &contact)
+{
+    this->name = contact.name;
+    this->email = contact.email;
+    this->phone = contact.phone;
+};
+
+string Contact::toString() const
+{
+    return "name = " + this->name + ", email = " + this->email + ", phone  = " + this->phone;
+};
+
+bool Contact::compare(const Contact &contact) const
+{
+    return (this->email == contact.email); // hamid wald lhram howa li galha lia
+}
+
+bool Contact::operator<(const Contact &contact) const
+{
+    return (this->name < contact.name);
+}
+void Contact::print() const
+{
+    std::cout << toString();
+}
