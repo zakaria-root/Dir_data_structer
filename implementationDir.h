a4768b1953329223c4eb4e3f4a6dc735d68132c6
zakaria-root <elmourtazakezia@gmail.com>
2022-04-09 16:56:39 +0000

    feat: add the dir implementaion

--- implementationDir.h
+++ implementationDir.h
@@ -1,14 +1,46 @@
 #include <iostream>
 #include "dir.h"
 
-Dir::Dir():size(0){};
+Dir::Dir() : size(0){};
 
-void Dir::add(string, string, string = ""){
-    
+void Dir::add(string name, string email, string phone = "")
+{
+    char c = email[0];
+    int position = toupper(c) - 65;
+    Contact contact(name, email, phone);
+    if (lists[position].isEmpty())
+        size++;
+    lists[position].add(contact);
 }
-// void remove(string, string = "");
-// Node *search(string, string = "") const;
-// void update(string, string, string, string = "");
-// string toString() const;
-// void print() const;
-// bool isEmpty() const;
+void Dir::remove(string name, string email = "")
+{
+    char c = email[0];
+    int position = toupper(c) - 65;
+    for (int index = position; index < NBLISTS - 1; index++)
+        lists[index] = lists[index + 1];
+    size--;
+}
+Node *Dir::search(string name, string email = "") const
+{
+    char c = email[0];
+    int position = toupper(c) - 65;
+    return lists[position].search(name, email);
+}
+void Dir::update(string searchEmail, string name, string email, string phone = "")
+{
+    char c = searchEmail[0];
+    int position = toupper(c) - 65;
+    lists[position].update(searchEmail, name, email, phone);
+}
+string Dir::toString() const
+{
+    return this->toString();
+}
+void Dir::print() const
+{
+    std::cout << toString();
+}
+bool Dir::isEmpty() const
+{
+    return size == 0;
+}
