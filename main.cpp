acc9b4962534187899f6648883578644c66dc44d
zakaria-root <elmourtazakezia@gmail.com>
2022-04-12 03:24:53 +0000

    feat: add amin implementation

--- main.cpp
+++ main.cpp
@@ -3,8 +3,18 @@
 #include "implementationDir.h"
 #include "implementationList.h"
 #include "implementationNode.h"
+#include "dir.h"
 
 main()
 {
+    
+    Dir dir;
+    dir.add("ahmed", "ahmed@gmail.com", "0665846999");
+    dir.add("ziko", "ziko@gmail.com", "0665846999");
+    dir.add("zakaria", "zakaria@gmail.com", "0665846999");
+    dir.add("bhima", "bhima@gmail.com", "0665846999");
+    dir.print();
+
+    
     std::cout << "the main file" << std::endl;
 }
